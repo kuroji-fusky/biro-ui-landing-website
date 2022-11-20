254ed062d2da98a547552d7b50b816977b96f11e
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-11-20 20:09:03 +0800

    Baby steps

---
+++ components/Container.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+interface ContainerProps {
+  title: string;
+  description?: string;
+  class?: string;
+}
+
+const props = defineProps<ContainerProps>();
+
+useHead({
+  title: props.title,
+  meta: [
+    { name: "description", content: props.description },
+    { property: "og:type", content: "website" },
+    { property: "og:title", content: props.title },
+    { property: "og:description", content: props.description },
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:title", content: props.title },
+    { name: "twitter:description", content: props.description },
+  ],
+});
+</script>
+
+<template>
+  <main id="page-container" :class="class">
+    <slot />
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const title = "Biro UI | A modern design system created by Kuroji Fusky";
+const desc = "Coming soon...";
+</script>
+
+<template>
+  <Container
+    :title="title"
+    :description="desc"
+    class="bg-gradient-to-t from-neutral-900 to-violet-900 text-white h-screen font-open-sans"
+  >
+    <article class="text-center pt-20">
+      <h2 class="font-inter text-3xl font-bold mb-5">Coming soon...</h2>
+      <p class="mb-3">The project is still incomplete!</p>
+      <p class="text-sm">ya dingus</p>
+    </article>
+  </Container>
+</template>
